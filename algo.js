a5d56ebcffaac7b161236661e7b8d8163950debb
manuhatho <amateur9525@gmail.com>
2024-07-15 11:56:53 +0100

    B

---
+++ algo.js
@@ -0,0 +1,11 @@
+function diviseur(){
+    var a = document.getElementById('nombre').value;
+    var nbre;
+    var resultElement = document.getElementById("result");
+    nbre = a % 2 ;
+    if(nbre===0){
+    if(NaN(a))
+
+    }
+diviseur();
+
