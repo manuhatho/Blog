1cd6e83c4bf9fecb95992f8d50b9e14ccf3e5d18
manuhatho <amateur9525@gmail.com>
2024-07-19 12:59:24 +0100

    G

--- footer.js
+++ footer.js
@@ -10,5 +10,15 @@ function footerReveal(){
             footer.classList.remove("reveal")
         }
     })
+    // window.addEventListener('scroll', () => {
+    //     const footer = document.querySelector('footer');
+    //     const revealPoint = document.documentElement.scrollHeight - window.innerHeight - footer.offsetHeight;
+        
+    //     if (window.scrollY >= revealPoint) {
+    //       footer.classList.add('reveal');
+    //     } else {
+    //       footer.classList.remove('reveal');
+    //     }
+    //   });
 }
 footerReveal()
