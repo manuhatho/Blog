0fbd7d6c7385a727de30ce25c52cbf079e982a0c
manuhatho <amateur9525@gmail.com>
2024-07-23 12:40:48 +0100

    j

--- body.js
+++ body.js
@@ -1,6 +1,28 @@
 function hoverGallery(){
 
-    const gallery = document.querySelector(".gallery");
-    console.log(gallery.lengh);
+   
+    const buttons = document.querySelectorAll("#btn");
+ 
+   
+    buttons.forEach((button,index)=>{
+       button.addEventListener("mouseover",()=>{
+            const galleryC = document.querySelectorAll(".galleryContent");
+            const indexGallery = galleryC[index];
+            indexGallery.classList.add("boxShadow");
+        
+       })
+       button.addEventListener("mouseout",()=>{
+        const galleryC = document.querySelectorAll(".galleryContent");
+        const indexGallery = galleryC[index];
+           
+            indexGallery.classList.remove("boxShadow");
+        
+       })
+
+
+        
+       
+        
+    })
 }
 hoverGallery()
